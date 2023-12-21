17bddcd4c9ad25c1f244358ddbfd449ad8457285
Rafal Tomczyk <rafal.tomczyk1992@gmail.com>
2023-12-21 19:36:48 +0100

    add basic project structure

---
+++ src/freertos_helpers/freertos_helper.c
@@ -0,0 +1,23 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "ch32v30x.h"
+#include "system_ch32v30x.h"
+
+/**
+ * @brief Initialize SysTick interrupt.
+ *
+ */
+void vPortSetupTimerInterrupt(void) {
+    /* Configure SysTick and interrupts. */
+    SysTick->SR = 0UL;
+    SysTick->CTLR = 0UL;
+    SysTick->CNT = 0UL;
+
+    NVIC_EnableIRQ(SysTicK_IRQn);
+
+    SysTick->CMP = configCPU_CLOCK_HZ / configTICK_RATE_HZ;
+    SysTick->CTLR = 0x1E; /* COUNTDOWN | AUTO RELOAD | HCLK/8 | INT */
+    SysTick->CTLR |= 0x20; /* INIT */
+    SysTick->CTLR |= 0x01; /* EN */
+}
---
+++ src/user_code/main.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "ch32v30x.h"
+#include "system_ch32v30x.h"
+
+extern void Fault_Handler(void);
+
+static void led_task(void *param)
+{
+    GPIO_InitTypeDef led_pin = {
+        .GPIO_Mode  = GPIO_Mode_Out_PP,
+        .GPIO_Speed = GPIO_Speed_50MHz,
+        .GPIO_Pin = GPIO_Pin_0
+    };
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_Init(GPIOC, &led_pin);
+
+    while (true) {
+        GPIO_SetBits(GPIOC, GPIO_Pin_0);
+        vTaskDelay(100);
+        GPIO_ResetBits(GPIOC, GPIO_Pin_0);
+        vTaskDelay(100);
+    }
+}
+
+int main(void) {
+    SystemInit();
+    SystemCoreClockUpdate();
+
+    xTaskCreate(led_task, "LED_TSK", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
+
+    vTaskStartScheduler();
+    
+    Fault_Handler();
+}
